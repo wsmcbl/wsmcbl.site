8fc155c2d195d87f18d6757f54f8af6b523d7229
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-05-08 16:04:40 -0600

    Update: app.routes.ts file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import {HomepageComponent} from './view/home/homepage/homepage.component';
 import {ViewGradeOnlineComponent} from './view/grade-online/view-grade-online/view-grade-online.component';
 
 export const routes: Routes = [
-  {path: 'as', component: HomepageComponent},
-  {path: '', component: ViewGradeOnlineComponent}
+  {path: '', component: HomepageComponent},
+  {path: 'view-grades', component: ViewGradeOnlineComponent}
 ];
