d391cbc79dfa19c361dd700daa0caab7745adcaa
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-05-08 14:31:29 -0600

    Update: view-grade-online.component

--- src/app/view/grade-online/view-grade-online/view-grade-online.component.ts
+++ src/app/view/grade-online/view-grade-online/view-grade-online.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, Injectable} from '@angular/core';
+import {ViewGradeOnlineController} from './view-grade-online.controller';
+import {StudentDto} from './student.dto';
+import {BasicStudentDto} from './basic-student.dto';
+import {Observable} from 'rxjs';
+import {AsyncPipe, NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+@Injectable({providedIn: 'root'})
 
 @Component({
   selector: 'app-view-grade-online',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    NgIf,
+    FormsModule
+  ],
   templateUrl: './view-grade-online.component.html',
   styleUrl: './view-grade-online.component.css'
 })
-export class ViewGradeOnlineComponent {
 
+export class ViewGradeOnlineComponent
+{
+  student?: Observable<StudentDto>;
+  studentDto: BasicStudentDto = {};
+
+  constructor(private controller : ViewGradeOnlineController)
+  {
+  }
+
+  getStudent(): void
+  {
+    this.student = this.controller.getStudent(this.studentDto!);
+  }
 }
