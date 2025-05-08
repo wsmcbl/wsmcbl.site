c6d8c189c60226c91b1b5747f18fd8cc5fe3c0c5
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-05-08 14:30:42 -0600

    Update: app.config and app.routes files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomepageComponent} from './view/home/homepage/homepage.component';
+import {ViewGradeOnlineComponent} from './view/grade-online/view-grade-online/view-grade-online.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'as', component: HomepageComponent},
+  {path: '', component: ViewGradeOnlineComponent}
+];
