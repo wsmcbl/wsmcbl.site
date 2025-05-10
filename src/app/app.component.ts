f1e8bbf3248f6ee8be3f678b315f8adb2085ad93
Ezequiel Urbina <ezequielurbina100@gmail.com>
2025-05-10 10:10:39 -0600

    Fix Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import { NavBarComponent } from './view/base/nav-bar/nav-bar.component';
-import {HeroComponent} from './view/base/hero/hero/hero.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, NavBarComponent, HeroComponent],
+    imports: [RouterOutlet, NavBarComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
