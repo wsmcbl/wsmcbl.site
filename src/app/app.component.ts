3ee522e4066fc268a2b3853ffd4dd32ee144c845
Ezequiel Urbina <ezequielurbina100@gmail.com>
2025-05-10 11:59:27 -0600

    Add: info.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import {RouterOutlet} from '@angular/router';
 import { NavBarComponent } from './view/base/nav-bar/nav-bar.component';
 import { FooterComponent } from './view/base/footer/footer.component';
 import {QuestionsComponent} from './view/base/questions/questions.component';
+import {InfoComponent} from './view/base/info/info.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, NavBarComponent, FooterComponent, QuestionsComponent],
+    imports: [RouterOutlet, NavBarComponent, FooterComponent, QuestionsComponent, InfoComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
